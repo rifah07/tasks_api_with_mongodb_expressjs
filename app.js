4a91d93e0bc2637f9ead397c4b48f1299db6d946
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-02-18 22:29:10 +0600

    movie post api added not completed

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 
+const addMovie = require("./controllers/addMovie");
+
 const app = express();
+
+app.post("/api/movies", addMovie);
 
 app.listen(8000, () => {
   console.log("Server started successfully!");
