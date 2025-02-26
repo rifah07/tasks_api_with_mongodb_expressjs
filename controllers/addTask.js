d9f9dd86370a42e83a3f526912023cc01b750619
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-02-26 16:00:40 +0600

    validation added

--- controllers/addTask.js
+++ controllers/addTask.js
@@ -1,7 +1,33 @@
 const addTask = (req, res) => {
   const { task_name, status } = req.body;
 
+  const validStatuses = ["pending", "completed"];
+
   //validations
+
+  if (!task_name) {
+    res.status(400).json({
+      status: "Failed!",
+      message: "Task name must be provided",
+    });
+    return;
+  }
+
+  if (!status) {
+    res.status(400).json({
+      status: "Failed!",
+      message: "Task status must be provided",
+    });
+    return;
+  }
+
+  if (!validStatuses.includes(status.toLowerCase())) {
+    res.status(400).json({
+      status: "Failed!",
+      message: "Task status must be 'Pending' or 'Completed'",
+    });
+    return;
+  }
 
   //successful
 
