0c55d3d9b8cccf30a977b168c29d8cac19e49047
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-03-03 23:49:55 +0600

    Update task done

--- controllers/editTask.js
+++ controllers/editTask.js
@@ -1,14 +1,33 @@
-const mongoose= require("mongoose");
+const mongoose = require("mongoose");
 
-const editTask = async (req, res) =>{
-      const tasksModel = mongoose.model("tasks");
+const editTask = async (req, res) => {
+  const tasksModel = mongoose.model("tasks");
+  const { task_id, task_name, status } = req.body;
 
-      res.status(200).json({
-        status: "This is from Edit TASK PART",
-        
+  try {
+    await tasksModel.updateOne(
+      {
+        _id: task_id,
+      },
+      {
+        task_name: task_name,
+      },
+      {
+        runValidators: true,
+      }
+    );
+  } catch (e) {
+    res.status(400).json({
+      status: "failed!",
+      message: e.message,
+    });
+    return;
+  }
 
-      })
+  res.status(200).json({
+    status: "Success",
+    message: "Task updated Successfully!",
+  });
+};
 
-}
-
-module.exports= editTask;
+module.exports = editTask;
