17d016813651631f7fc37b2c4b5dcae0dfdf9ecd
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-03-05 20:25:12 +0600

    deletion and get songle task part updated

--- controllers/deleteTask.js
+++ controllers/deleteTask.js
@@ -1,29 +1,39 @@
-const mongoose= require("mongoose");
+const mongoose = require("mongoose");
 
-const deleteTask = async(req,res) =>{
+const deleteTask = async (req, res) => {
+  const tasksModel = mongoose.model("tasks");
+  const task_id = req.params.task_id;
 
-    const tasksModel = mongoose.model("tasks");
-    const task_id= req.params.task_id;
+  const getTask = await tasksModel.findOne({
+    _id: task_id,
+  });
 
-    try{
-        await tasksModel.deleteOne({
-            _id: task_id
-        })
+  try {
+    if (!getTask) throw "Task not found!";
+  } catch (e) {
+    res.status(400).json({
+      status: "Failed deletion!",
+      message: e,
+    });
+    return;
+  }
 
-    }catch(e){
-        res.status(400).json({
-            status: "Failed!",
-            message: e.message
-        });
-        return;
-    }
+  try {
+    await tasksModel.deleteOne({
+      _id: task_id,
+    });
+  } catch (e) {
+    res.status(400).json({
+      status: "Failed!",
+      message: e.message,
+    });
+    return;
+  }
 
+  res.status(200).json({
+    status: "Successfulll",
+    message: "Task deleted successfully!",
+  });
+};
 
-    res.status(200).json({
-        status: "Successfulll",
-        message: "Task deleted successfully!",
-    })
-
-}
-
-module.exports= deleteTask;
+module.exports = deleteTask;
--- controllers/getSingleTask.js
+++ controllers/getSingleTask.js
@@ -3,19 +3,26 @@ const mongoose = require("mongoose");
 const getSingleTask = async (req, res) => {
   const tasksModel = mongoose.model("tasks");
 
+
+
   try {
     const tasksData = await tasksModel.findOne({
       _id: req.params.task_id,
     });
+    if (!tasksData) {
+      throw "There is no task like this";
+    }
+    else {
+      res.status(200).json({
+        status: "Data is here!",
+        message: tasksData,
 
-    res.status(200).json({
-      status: "Data is here!",
-      message: tasksData,
-    });
+      });
+    }
   } catch (e) {
     res.status(400).json({
       status: "Failed",
-      data: e.message,
+      message: e.message,
     });
     return;
   }
