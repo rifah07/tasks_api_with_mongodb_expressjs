dd99c8be1f0911cab96a12ce5bf68a04d4aa2457
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-03-09 00:03:03 +0600

    errors resolved and openai connected

--- controllers/taskFilter.js
+++ controllers/taskFilter.js
@@ -1,19 +1,19 @@
-import OpenAI from 'openai';
+const OpenAI = require('openai');
 
-const taskFilter = (req, res) => {
+const taskFilter = async (req, res) => {
 
     const client = new OpenAI({
         apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
       });
 
-      function main() {
+      async function main() {
         const chatCompletion = await client.chat.completions.create({
           messages: [{ role: 'user', content: 'Say this is a test' }],
           model: 'gpt-4o',
         });
       }
       
-      main();
+      await main();
 
     res.status(200).json({
         status: "Greetings from Open AI"
