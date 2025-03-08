4337071a2112e8ff5dbb6d21bf37e84429bc9de1
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-03-08 23:57:45 +0600

    api added

--- controllers/taskFilter.js
+++ controllers/taskFilter.js
@@ -1,4 +1,12 @@
+import OpenAI from 'openai';
+
 const taskFilter = (req, res) => {
+
+    const client = new OpenAI({
+        apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
+      });
+
+      
     res.status(200).json({
         status: "Greetings from Open AI"
     })
