cc50367c59d81a6b5c41a6128d0d2bde414a6d37
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-03-08 23:59:26 +0600

    response of ai added

--- controllers/taskFilter.js
+++ controllers/taskFilter.js
@@ -6,7 +6,15 @@ const taskFilter = (req, res) => {
         apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
       });
 
+      function main() {
+        const chatCompletion = await client.chat.completions.create({
+          messages: [{ role: 'user', content: 'Say this is a test' }],
+          model: 'gpt-4o',
+        });
+      }
       
+      main();
+
     res.status(200).json({
         status: "Greetings from Open AI"
     })
