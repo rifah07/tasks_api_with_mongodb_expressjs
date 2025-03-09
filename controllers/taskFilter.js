f106b5ac718b9d48072cb33363a6957dfb2e0f56
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-03-09 22:38:12 +0600

    new package installed and errors handled

--- controllers/taskFilter.js
+++ controllers/taskFilter.js
@@ -1,19 +1,19 @@
-const OpenAI = require('openai');
+//const OpenAI = require('openai');
 
 const taskFilter = async (req, res) => {
 
-    const client = new OpenAI({
-        apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
-      });
+   // const client = new OpenAI({
+    //    apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
+   //   });
 
-      async function main() {
-        const chatCompletion = await client.chat.completions.create({
-          messages: [{ role: 'user', content: 'Say this is a testfor OpenAI' }],
-          model: 'gpt-3.5-turbo',
-        });
-      }
+  //    async function main() {
+  //      const chatCompletion = await client.chat.completions.create({
+    //      messages: [{ role: 'user', content: 'Say this is a testfor OpenAI' }],
+    //      model: 'gpt-3.5-turbo',
+     //   });
+     // }
       
-      await main();
+    //  await main();
 
     res.status(200).json({
         status: "Greetings from Open AI"
